9bff0495d37edd84cdd04adf4f26d13047c41566
alpen <zy15626299617@gmail.com>
2020-04-27 16:31:14 +0800

    更新

--- src/com/tag/array/plusOne.java
+++ src/com/tag/array/plusOne.java
@@ -10,8 +10,29 @@ package com.tag.array;
 //输出: [1,2,4]
 //解释: 输入数组表示数字 123。
 
-public class plusOne {
-    public static void main(String[] a){
+import java.util.Arrays;
 
+public class plusOne {
+    public static void main(String[] a) {
+        int[] nums = new int[]{9,9,9,9};
+
+        class solution {
+            public int[] get(int[] nums) {
+                for (int i = nums.length - 1; i > -1; i--) {
+                    //数组最后一位数加一
+                    nums[i]++;
+                    //如果递增后除于十余数不为零，结束循环
+                    if (nums[i] % 10 != 0) return nums;
+                    //如果递增后除于十余数为零，该元素为9，更新为零，继续循环
+                    nums[i] = 0;
+                }
+                //若for循环执行完毕，说明数字为99，999这类数字，则手工处理
+                nums = new int[nums.length+1];
+                nums[0] = 1;
+                return nums;
+            }
+        }
+
+        System.out.println(Arrays.toString(new solution().get(nums)));
     }
 }
