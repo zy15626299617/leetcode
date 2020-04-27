ee4214714a8d6b36dc343d53f1316915fede0cbd
alpen <zy15626299617@gmail.com>
2020-04-27 17:04:14 +0800

    更新

---
+++ src/com/tag/array/merge.java
@@ -0,0 +1,25 @@
+package com.tag.array;
+
+//leedcode:88
+
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+//初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+//你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//输出: [1,2,2,3,5,6]
+
+public class merge {
+    public static void main(String[] a){
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        int[] nums2 = new int[]{2,5,6};
+
+        int i,j = 0;
+        while(i < nums1.length){
+            while(j < nums2.length){
+                if (nums2[j] >)
+            }
+        }
+    }
+}
