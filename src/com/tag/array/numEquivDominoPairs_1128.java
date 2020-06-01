bf7f913e8389867c8cc2114e072bd9b2526a2164
alpen <zy15626299617@gmail.com>
2020-06-01 17:06:59 +0800

    更新

--- src/com/tag/array/numEquivDominoPairs_1128.java
+++ src/com/tag/array/numEquivDominoPairs_1128.java
@@ -1,7 +1,27 @@
 package com.tag.array;
 
+import java.util.Arrays;
+
 public class numEquivDominoPairs_1128 {
     public static void main(String[] args){
 //使用桶思想
+        int[][] dominoes = {};
+
+        int count = 0;
+        int[] keg = new int[100];
+        for (int[] i : dominoes){
+            Arrays.sort(i);
+            keg[i[0]*10+i[1]]++;
+        }
+
+        for (int i : keg) count+= i*(i-1)/2;
+
+        //优化版
+        //int count = 0;
+        //int[] keg = new int[100];
+        //for (int[] i : dominoes){
+        //    Arrays.sort(i);
+        //    count+=keg[i[0]*10+i[1]]++;
+        //}
     }
 }
--- src/com/tag/array/relativeSortArray_1122.java
+++ src/com/tag/array/relativeSortArray_1122.java
@@ -3,5 +3,23 @@ package com.tag.array;
 public class relativeSortArray_1122 {
     public static void main(String[] args){
 //使用桶思想
+        int[] arr1 = {},arr2 = {};
+
+        int[] keg = new int[1001];
+        for (int i : arr1) keg[i]++;
+
+        int index = 0;
+        for (int i : arr2) {
+            while (keg[i]-- > 0){
+                arr1[index++] = i;
+            }
+        }
+
+        for (int i = 0; i < 1001; i++) {
+            while (keg[i]-- > 0){
+                arr1[index++] = i;
+            }
+        }
+
     }
 }
